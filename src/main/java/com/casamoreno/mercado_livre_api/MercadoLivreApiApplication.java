58ab2acae6a5057b9c43e654e1ef021711b68f80
Matteus Moreno <matteus_moreno@live.com>
2025-06-28 08:41:35 -0300

    initial commit

---
+++ src/main/java/com/casamoreno/mercado_livre_api/MercadoLivreApiApplication.java
@@ -0,0 +1,13 @@
+package com.casamoreno.mercado_livre_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoLivreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoLivreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/casamoreno/mercado_livre_api/MercadoLivreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casamoreno.mercado_livre_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoLivreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
